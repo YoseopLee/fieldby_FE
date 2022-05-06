1abaea1c14e9546a3b4ee7a3da555da45911ad1a
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-05-06 18:05:39 +0900

    add/styled

---
+++ src/Login/PreLogin.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const PreLogin = () => {
+    return (
+        <PreLoginCSS>
+            
+        </PreLoginCSS>
+    )
+}
+
+const PreLoginCSS = styled.div`
+
+`
+
+export default PreLogin;
