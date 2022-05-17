5171421171c029fdc2944a96cd5ad91e882eec1f
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-05-17 17:56:33 +0900

    회원가입

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { authService } from "../../fBase";
+import { authService, realtimeDbService } from "../../fBase";
+import { ref, set } from "firebase/database";
 
 const SignUp = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
     const [registerEmail, setRegisterEmail] = useState('');
     const [registerPassword, setRegisterPassword] = useState('');
+    const [confirmRegisterPassword, setConfirmRegisterPassword] = useState('');
+    const [companyKind, setCompanyKind] = useState('');
+    const [name, setName] = useState('');
+    const [companyName, setCompanyName] = useState('');
 
+   function writeUserData () {
+       const uid = authService.currentUser.uid;
+       set(ref(realtimeDbService, `brands/${uid}`), {
+            phoneNum : phoneNumber,
+            email : registerEmail,
+            companyKind : companyKind,
+            name : name,
+            companyName : companyName,
+            
+       })
+   }
+    
     const register = async() => {
         try {
             const user = await createUserWithEmailAndPassword(
@@ -15,6 +33,9 @@ const SignUp = () => {
                 registerPassword
             );
             console.log(user);
+            writeUserData();
+            window.alert('가입완료');
+            window.location.href = "/login";
         } catch (error) {
             console.log(error.message);
         }
@@ -22,20 +43,230 @@ const SignUp = () => {
 
     return (
         <SignUpContainerCSS>
-            <input placeholder="이메일" onChange={(e) => {
-                setRegisterEmail(e.target.value);
-            }}
-            />
-            <input placeholder="비밀번호" onChange={(e) => {
-                setRegisterPassword(e.target.value);
-            }}/>
-            <button onClick={register}>회원가입</button>
+            <div className="sign-up-square-hc sign-up-square-vc" id="sign-up-cm">
+                <div className="sign-up-logo">
+                    <img src="images/필드바이 로고-47 1.png" alt=""/>
+                    <h2 className="sign-up-logo-name">Business Suite</h2>
+                </div>
+                <div className="sign-up-title">회원가입</div>
+                <div className="sign-up-box">
+                    <span className="sign-up-name">휴대폰 번호</span>
+                    <div className="sign-up-phone-wrapper">
+                        <input className="sign-up-phone" placeholder="핸드폰" value={phoneNumber} onChange={(e) => {
+                            setPhoneNumber(e.target.value);
+                        }}/>
+                        <button className="sign-up-phone-btn">인증번호 발송</button>
+                    </div>
+                    
+                </div>
+                <div className="sign-up-box">
+                    <span className="sign-up-name">이메일</span>
+                    <input className="sign-up-input" type="email" placeholder="이메일" onChange={(e) => {
+                        setRegisterEmail(e.target.value);
+                    }}/>
+                </div>
+                <div className="sign-up-box">
+                    <span className="sign-up-name">비밀번호</span>
+                    <input className="sign-up-input sign-up-input-pw" type="password" placeholder="새 비밀번호" onChange={(e) => {
+                        setRegisterPassword(e.target.value);
+                    }}/>
+                    <input className="sign-up-input" type="password" placeholder="비밀번호 확인" onChange={(e) => {
+                        setConfirmRegisterPassword(e.target.value)
+                    }}/>
+                </div>
+                <div className="sign-up-box">
+                    <span className="sign-up-name">기업구분</span>
+                    <select className="sign-up-kind" name="company_kind" onChange={(e) => {
+                        setCompanyKind(e.target.value);
+                    }}> 
+                        <option value="">선택해주세요.</option>
+                        <option value="일반">일반</option>
+                        <option value="에이전시">에이전시</option>
+                        <option value="소상공인">소상공인</option>
+                    </select>
+                </div>
+                
+                <div className="sign-up-box">
+                    <span className="sign-up-name">담당자 성함 / 직함</span>
+                    <input className="sign-up-input" type="text" placeholder="담당자명" onChange={(e) => {
+                        setName(e.target.value);
+                    }}/>
+                </div>
+                <div className="sign-up-box">
+                    <span className="sign-up-name">회사명 정보</span>
+                    <input className="sign-up-input" type="text" placeholder="회사 or 브랜드명" onChange={(e) => {
+                        setCompanyName(e.target.value);
+                    }}/>
+                </div>
+                <div className="register-box">
+                    <span className="register-confirm">필드바이의 이용약관 및 개인정보취급방침에 동의합니다.</span>
+                    <button id="register-btn" onClick={register}>가입 완료하기</button>
+                </div>
+                <div className="register-info-container">
+                    <div className="register-info-wrapper">
+                        <a href="/privacy" className="register-privacy">개인정보처리방침</a>
+                        <span>|</span>
+                        <a href="/policy" className="register-policy">이용약관</a>
+                    </div>
+                    <span className="register-info-rights">ⓒ 2022 FIELDBY Corporation. All Rights Reserved.</span>
+                </div>
+            </div>
+            
         </SignUpContainerCSS>
     );
 };
 
 const SignUpContainerCSS = styled.div`
-
+    #sign-up-cm {
+        position : absolute;
+        min-width : 672px;
+        min-height : 850px;
+        display : flex;
+        flex-direction : column;
+        input {
+            border : none;
+            height : 48px;
+            background: #F1F1F1;
+            border-radius: 13px;
+        }
+    }
+    .sign-up-square-hc {
+        width : 30%;
+        left : 0;
+        right : 0;
+        margin-left : auto;
+        margin-right : auto;
+        background : #ffffff;
+        box-shadow : 2px 2px 10px rgba(0,0,0, 0.07);
+        border-radius : 5px;   
+    }
+    .sign-up-square-vc {
+        top : 20px;
+        bottom : 20px;
+        margin-top : auto;
+        margin-bottom : auto;
+    }
+    .sign-up-logo {
+        margin-top : 20px;
+        margin-left : 16px;
+        img {
+            height : 42px;
+            width : 133px;
+        }
+        .sign-up-logo-name {
+            margin-left : 14px;
+            margin-block-start : -10px;
+            margin-block-end : 0;
+            font-size : 18px;
+            color : #303030;
+        }
+    }
+    .sign-up-title {
+        font-size : 20px;
+        font-weight : bold;
+        text-align : center;
+        margin-bottom : 20px;
+    }
+    .sign-up-box {
+        display : flex;
+        flex-direction : column;
+        margin-left : 120px;
+        margin-right : 120px;
+        justify-content : center;
+        margin-bottom : 12px;
+        .sign-up-name {
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 22px;
+            margin-bottom : 8px;
+        }
+        .sign-up-input {
+            padding-left : 12px;
+        }
+        .sign-up-phone {
+            padding-left : 12px;
+            width : 65%;
+            margin-right : 8px;
+        }
+        .sign-up-phone-btn {
+            background: #303030;
+            border-radius: 13px;
+            height : 48px;
+            color : #ffffff;
+            font-weight : 700;
+            line-height : 19px;
+            width : 120px;
+            text-align : center;
+            border : none;
+        }
+        .sign-up-input-pw {
+            margin-bottom : 8px;
+        }
+        
+    }
+    .sign-up-kind {
+        border: 1px solid #303030;
+        border-radius: 13px;
+        height : 48px;
+        padding-left : 12px;
+    }
+    .register-box {
+        left: 0;
+        right : 0;
+        margin-left : auto;
+        margin-right : auto;
+        display : flex;
+        flex-direction : column;
+        text-align : center;
+        margin-top : 8px;
+    }
+    .register-confirm {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        color: #22BAA8;
+    }
+    #register-btn {
+        background: #22BAA8;
+        border-radius: 5px;
+        height : 46px;
+        margin-top : 6px;
+        width : 230px;
+        border : none;
+        color : #ffffff;
+        font-weight : 700;
+        line-height : 19px;
+    }
+    .register-info-container {
+        margin-top : 16px;
+        display : grid;
+        justify-content : center;
+        text-align : center;
+        .register-info-wrapper {
+            font-style: normal;
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 17px;
+            .register-privacy {
+                margin-right : 3px;
+                text-decoration : none;
+                color : #8e8e8e;
+            }
+            .register-policy {
+                margin-left : 3px;
+                text-decoration : none;
+                color : #8e8e8e;
+            }
+        }
+        
+        .register-info-rights {
+            color : #8e8e8e;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 17px;
+        }
+    }
 `
 
 export default SignUp;
